36c82188a199cd80e34af5015a12b3f6ed8c69b5
NateLosee <93952257+NateLosee@users.noreply.github.com>
2021-12-01 18:25:20 -0700

    Create Final_Project_2.R
    I fixed the Sentiment stuff, so now only Positive and Negative are showing

---
+++ Final_Project_2.R
@@ -0,0 +1,161 @@
+library(ROAuth)
+library(tm)
+library(syuzhet)
+library(twitteR)
+library('SnowballC')
+
+#set up to authenticate
+consumer_key = "pXwwnWa9fQQ74uuHsm9GXTim7"
+consumer_secret = "bMPJNzwU4zz2XxowAHxPHzdBvtYQkeC3cu5Nvp2AFRfUiJwniC" 
+access_token = "427051006-iu9A48vwbFJJoWDFOiTcK4DrT78KS5SGd2kMQTZG"
+access_secret = "zBBzUr9ZnoJ4UOkNwJF9bwqhTYzjU0GPoPZWCMVQwAUdy"
+setup_twitter_oauth(consumer_key ,consumer_secret,access_token ,access_secret)
+
+
+APX <- searchTwitter("#APEX", n=200)
+APX =  strip_retweets(APX)
+
+FTN <- searchTwitter("#Fortnite", n=200)
+FTN =  strip_retweets(FTN)
+
+VLT <- searchTwitter("#Valorant", n=200)
+VLT =  strip_retweets(VLT)
+
+clean = function(text){
+  text = tolower(text)
+  text = gsub("@\\w+", "", text)
+  text = gsub("[[:punct:]]", "", text)#remove punctuation
+  text = gsub("http\\w+","", text)#remove links
+  text = gsub("[ |\t]{2,}", "", text)#remove tabs
+  text = gsub("^ ", "", text)#remove blank spaces
+  text = gsub(" $", "", text)#remove blank spaces
+  text = removeWords(text  ,  stopwords("en") )#remove stop words
+  text = stemDocument(text)#Stems the document
+  
+  return(text)
+}
+
+df.APX <- twListToDF(APX)
+df.FTN <- twListToDF(FTN)
+df.VLT <- twListToDF(VLT)
+
+head(df.VLT)
+
+APX.text = clean(df.APX$text)
+VLT.text = clean(df.VLT$text)
+FTN.text = clean(df.FTN$text)
+
+
+
+sentiment.info = function(text){ 
+  
+  text.sentiment = get_nrc_sentiment((text))
+  sentiment.score = data.frame(colSums(text.sentiment[,]))
+  names(sentiment.score) = 'Score'
+  sentiment.score = cbind("sentiment" = rownames(sentiment.score), sentiment.score)
+  rownames(sentiment.score) = NULL
+  
+  return(sentiment.score)
+}
+
+
+sentiment.APX = sentiment.info(APX.text)
+sentiment.APX[9:10,]
+
+#Graph sentiment
+library(ggplot2)
+ggplot(data = sentiment.APX[9:10,], aes(x=sentiment, y=Score))+geom_bar(aes(fill=sentiment), stat = 'identity' )+
+  theme(legend.position = "none") +
+  xlab("Sentiments")+ylab("scores") + ggtitle("Sentiments")
+
+
+sentiment.VLT = sentiment.info(VLT.text)
+sentiment.VLT[9:10,]
+#Graph sentiment
+ggplot(data = sentiment.VLT[9:10,], aes(x=sentiment, y= Score))+geom_bar(aes(fill=sentiment), stat = 'identity' )+
+  theme(legend.position = "none") +
+  xlab("Sentiments")+ylab("scores") + ggtitle("Sentiments")
+
+
+sentiment.FTN = sentiment.info(FTN.text)
+sentiment.FTN[9:10,]
+#Graph sentiment
+ggplot(data = sentiment.FTN[9:10,], aes(x=sentiment, y= Score))+geom_bar(aes(fill=sentiment), stat = 'identity' )+
+  theme(legend.position = "none") +
+  xlab("Sentiments")+ylab("scores") + ggtitle("Sentiments")
+
+##########
+
+APX.corpus = Corpus(VectorSource(APX.text))
+VLT.corpus = Corpus(VectorSource(VLT.text))
+FTN.corpus = Corpus(VectorSource(FTN.text))
+
+#Word cloud
+#install.packages('wordcloud')
+library(wordcloud)
+wordcloud(APX.corpus, min.freq = 2, colors = brewer.pal(8,"Dark2"),max.words = 1000)
+wordcloud(VLT.corpus, min.freq = 2, colors = brewer.pal(8,"Dark2"),max.words = 1000)
+wordcloud(FTN.corpus, min.freq = 2, colors = brewer.pal(8,"Dark2"),max.words = 1000)
+
+
+
+
+corpus = Corpus(VectorSource(c(APX.text,FTN.text, VLT.text)))
+
+y= as.factor(c( rep('APX', length(APX.text)) , rep('FTN', length(FTN.text)),rep('VLT', length(VLT.text))  ))
+
+
+
+DTM <- DocumentTermMatrix(corpus)
+inspect(DTM)
+sparse_DTM <- removeSparseTerms(DTM, 0.97)
+inspect(sparse_DTM)
+tweets <- as.data.frame(as.matrix(sparse_DTM))
+#tweets <- as.data.frame(as.matrix(tweets))
+colnames(tweets) <- make.names(colnames(tweets))
+dim(tweets)
+
+#Tree classification
+library(tree)
+
+tweets = data.frame(y, tweets)
+head(tweets)
+
+train.index <- sample(c(1:dim(tweets)[1]), floor( 0.7 * dim(tweets)[1] ), replace = FALSE)
+
+tweets.test = tweets[-train.index,]
+
+tree.tweets = tree(y ~.-y,data = tweets, subset = train.index)
+tree.pred = predict(tree.tweets,tweets.test, type = "class")
+
+table(tree.pred,tweets.test$y)
+
+
+plot(tree.tweets)
+text(tree.tweets)
+
+#Pruning
+cv.tweets = cv.tree(tree.tweets, FUN = prune.misclass)
+cv.tweets
+#Size is the number of terminal nodes of each tree considered
+#Dev is the cross validation error rate 
+
+plot(cv.tweets$size, cv.tweets$dev,type = "b")
+#plot(cv.tweets$k, cv.tweets$dev,type = "b")
+
+#Prune the tree
+prune.tweets = prune.misclass(tree.tweets,best = 3) #Choose the size that corresponds to the lowest dev
+plot(prune.tweets)
+text(prune.tweets)
+
+tree.pred = predict(prune.tweets,tweets.test, type = 'class')
+table(tree.pred,tweets.test$y)
+##
+
+knn.pred = knn(train.x,test.x,train.y,k=10)
+#Is this a good model?
+table(knn.pred, test.y)
+
+library('class')
+knn.pred = glm(prune.tweets,tweets.test, family='binomial')
+table(knn.pred,tweets.test$y)
